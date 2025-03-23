e8fdae68a5a33a6b077366779648d69ff3da9c8b
Waruna Weerasinghe <it22578846@my.sliit.lk>
2025-03-23 12:32:20 +0530

    Create LoyaltyForm
    Create a Loyalty form and install the bootstrap files and modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Header from './components/Loyalty/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div>
+      <Header/>
     </div>
   );
 }
