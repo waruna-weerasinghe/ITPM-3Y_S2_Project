129474777b77d020752e19e0b03bf971d50ed39b
DilukaB <navanjana721@gmail.com>
2025-03-24 09:07:04 +0530

    Update ClothesCard.jsx

--- frontend/src/pages/home/clothes/ClothesCard.jsx
+++ frontend/src/pages/home/clothes/ClothesCard.jsx
@@ -15,7 +15,7 @@ const ClothesCard = ({ clothe }) => {
             />
             <div className="p-5 space-y-3">
                 <h3 className="text-2xl font-semibold text-gray-900 truncate hover:text-indigo-600 transition-colors duration-200">{clothe.title}</h3>
-                <p className="text-gray-500 text-sm">{clothe.category}</p>
+
                 <p className="text-gray-900 font-bold text-xl">${clothe.price}</p>
                 <button className="mt-5 w-full bg-indigo-600 hover:bg-indigo-700 text-white py-3 rounded-lg font-semibold shadow-md hover:shadow-xl transition-all duration-200 ease-in-out flex items-center justify-center gap-2 transform hover:scale-105">
                     <span>Add to Cart</span> <span className="text-lg">🛒</span>
