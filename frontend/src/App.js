9645970ec6c8d80ef2c8428daeb380d2afc6943b
Rushan Avishka <rushanavishka4@gmail.com>
2025-03-26 19:08:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,10 @@ import LoyaltyAddForm from './components/Loyalty/LoyaltyAddForm';
 import AllLoyaltyForm from './components/Loyalty/AllLoyaltyForms.js';
 import UpdateLoyalty from './components/Loyalty/UpdateLoyalty';
 import DeleteLoyaltyForm from './components/Loyalty/DeleteLoyalty.js';
-import Home from './pages/home/Home';
+import Home from './pages/home/Home.jsx';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
+import Cart from './Cart/Cart.jsx';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         <Route path="/addForm" element={<LoyaltyAddForm />} />
         <Route path="/updateLoyalty/:id" element={<UpdateLoyalty />} />
         <Route path="/deleteLoyaltyForm/:id" element={<DeleteLoyaltyForm />} />
-       
+        <Route path="/cart" element={<Cart />} />
       </Routes>
 
       <footer>Footer</footer>
