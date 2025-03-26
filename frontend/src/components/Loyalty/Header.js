af648fa4d8e02851bbae9dd91181b7273de026fe
Rushan Avishka <rushanavishka4@gmail.com>
2025-03-26 19:07:53 +0530

    Update Header.js

--- frontend/src/components/Loyalty/Header.js
+++ frontend/src/components/Loyalty/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
 
   return (
     <ul className="nav">
       <li className="nav-item">
-        <a className="nav-link active" aria-current="page" href="">Home</a>
+        <a className="nav-link active" aria-current="page" href="/">Home</a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="/addForm">LoyaltyForm</a>
@@ -14,7 +15,30 @@ function Header() {
         <a className="nav-link" href="list">List</a>
       </li>
 
+      <Link to="/cart">
+        
+        <div className="nav-bag">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="35"
+                height="35"
+                fill="currentColor"
+                className="bi bi-cart3"
+                viewBox="0 0 16 16"
+            >
+        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+    </svg>
+    <span className="bag-quantity">
+        <span>3</span>
+    </span>
+
+        </div>
+        </Link>
+
     </ul>
+
+
+
   )
 
 }
