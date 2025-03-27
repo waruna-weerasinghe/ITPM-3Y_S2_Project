00338cf6ecb77fe0690ef51c8ea69ac52d8fba56
DilukaB <navanjana721@gmail.com>
2025-03-27 20:21:53 +0530

    add

--- frontend/src/components/Loyalty/Header.js
+++ frontend/src/components/Loyalty/Header.js
@@ -19,27 +19,27 @@ function Header() {
       </li>
 
       <Link to="/cart">
-        
+
         <div className="nav-bag">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="35"
-                height="35"
-                fill="currentColor"
-                className="bi bi-cart3"
-                viewBox="0 0 16 16"
-            >
-        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-    </svg>
-    <span className="bag-quantity">
-        <span>3</span>
-    </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            fill="currentColor"
+            className="bi bi-cart3"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+          </svg>
+          <span className="bag-quantity">
+            <span>3</span>
+          </span>
 
         </div>
-        </Link>
+      </Link>
 
       <li className="nav-item">
-        <a className="nav-link" href="admin">Product Admin</a>
+        <a className="nav-link" href="dashboard">Product Admin</a>
       </li>
 
     </ul>
