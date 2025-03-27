8ab130ea17ac98b51b47f287a3f13fd180a2016e
Waruna Weerasinghe <it22578846@my.sliit.lk>
2025-03-28 00:44:03 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<AdminDashboard />} />
-
+<ho></ho>
       </Routes>
 
       
